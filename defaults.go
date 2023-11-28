ab57da08c0ffe3723534ebc9e9323fe56afdfe0a
pdaambrosio <pda.ambrosio@gmail.com>
2023-11-27 23:58:54 -0300

    feat: defaults.go doc

--- defaults.go
+++ defaults.go
@@ -2,9 +2,11 @@ package main
 
 var itemDiscount int = 10 // global variable
 
-// The function "changeDiscount" updates the value of the "itemDiscount" variable and returns the
-// updated value. Is important use the convention of the name of the function, in this case the name of the function is "DefaultsChangeDiscount"
-// because Default is the name of the file and ChangeDiscount is the name of the function.
+/*
+ The function "changeDiscount" updates the value of the "itemDiscount" variable and returns the
+ updated value. Is important use the convention of the name of the function, in this case the name of the function is "DefaultsChangeDiscount"
+ because Default is the name of the file and ChangeDiscount is the name of the function.
+*/
 func DefaultsChangeDiscount(discount int) int {
 	itemDiscount = discount
 	return itemDiscount
